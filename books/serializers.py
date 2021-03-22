924c85e57d25de0534f371e61fe12935335d44c7
Maria Chacko <73102673+mariachacko93@users.noreply.github.com>
2021-03-22 15:18:25 +0400

    Update serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -36,24 +36,24 @@ class BookSerializer(serializers.HyperlinkedModelSerializer):
 
     
 #    using try block
-    def update(self, instance, validated_data):
-        authors_list = []
-        try:
-            author = validated_data.pop('author')
-            for author in author:
-                print(author)
-                author, created = Author.objects.get_or_create(author=author['author'])
-                authors_list.append(author)
-        except:
-            for i in instance.author.all():
-                authors_list.append(i)
-        instance.author.set(authors_list)
-        instance.bookname = validated_data.get('bookname', instance.bookname)
-        instance.pubyear=validated_data.get('pubyear', instance.pubyear)
-        instance.pages = validated_data.get('pages', instance.pages) 
-        instance.abstract = validated_data.get('abstract', instance.abstract)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         authors_list = []
+#         try:
+#             author = validated_data.pop('author')
+#             for author in author:
+#                 print(author)
+#                 author, created = Author.objects.get_or_create(author=author['author'])
+#                 authors_list.append(author)
+#         except:
+#             for i in instance.author.all():
+#                 authors_list.append(i)
+#         instance.author.set(authors_list)
+#         instance.bookname = validated_data.get('bookname', instance.bookname)
+#         instance.pubyear=validated_data.get('pubyear', instance.pubyear)
+#         instance.pages = validated_data.get('pages', instance.pages) 
+#         instance.abstract = validated_data.get('abstract', instance.abstract)
+#         instance.save()
+#         return instance
    
    
    
